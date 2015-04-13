afd71a23927a10b38247f161f7f94ac68e2513c1
Ravinder Kadiyan <ravinder.kadiyan@siemens.com>
2015-04-13 15:28:18 +0200

    initial

---
+++ src/com/home/problem/Problem1.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) Siemens AG 2015 ALL RIGHTS RESERVED.
+ *
+ * R8  
+ * 
+ */
+
+package com.home.problem;
+
+/**
+ * 
+ * If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+ * 
+ * Find the sum of all the multiples of 3 or 5 below 1000.
+ * 
+ * Answer:233168
+ * 
+ */
+public class Problem1
+{
+    public static void main(String[] args)
+    {
+        /*
+         * use formula number*n*(n-1)/2
+         */
+        int N = 999;
+        int threeNumbers = 3 * (N / 3) * (N / 3 + 1) / 2;
+        int fiveNumbers = 5 * (N / 5) * (N / 5 + 1) / 2;
+        int fifteenNumbers = 15 * (N / 15) * (N / 15 + 1) / 2;
+        System.out.println("Sum is : " + (threeNumbers + fiveNumbers - fifteenNumbers));
+    }
+}
+
+
+/*
+ * Copyright (c) Siemens AG 2015 ALL RIGHTS RESERVED
+ *
+ * R8
+ */
